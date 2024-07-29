92222c2ae45e20b8998bcfa65c19fea7089ac377
Maycon J Pereira <mayconjovan@gmail.com>
2024-07-29 15:47:41 -0300

    Resolvendo o Lista Tarefa

---
+++ src/list/ListaTarefa.java
@@ -0,0 +1,56 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+
+
+    private List<Tarefa> listTarefa;
+
+
+    public ListaTarefa() {
+        this.listTarefa = new ArrayList<>();
+    }
+
+
+    public void adicionarTarefa(String descricao){
+        listTarefa.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for(Tarefa t : listTarefa) {
+            if(t.getDescription().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(t);
+            }
+        }
+        listTarefa.removeAll(tarefasParaRemover);
+    }
+
+
+    public int obterNumeroTotalTarefas() {
+        return listTarefa.size();
+    }
+
+    public void obterDescricoesTarefas() {
+        System.out.println(listTarefa);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+        System.out.println("O numero total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+
+        System.out.println("O numero total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("Tarefa 1");
+        System.out.println("O numero total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricoesTarefas();
+    }
+
+}
---
+++ src/list/Tarefa.java
@@ -0,0 +1,20 @@
+package list;
+
+public class Tarefa {
+    private String description;
+
+
+    public Tarefa(String description) {
+        this.description = description;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+}
