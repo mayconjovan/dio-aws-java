4acc0632b1618eb1d26caaa4e343eb9d590e4437
Maycon J Pereira <mayconjovan@gmail.com>
2024-07-30 15:57:43 -0300

    Resolvendo o Lista Tarefa

--- src/list/Search/CatalogoLivros.java
+++ src/list/Search/CatalogoLivros.java
@@ -42,4 +42,35 @@ public class CatalogoLivros {
         return livrosPorIntervaloAno;
     }
 
+    public Livro pesquisaPorTitulo(String titulo) {
+        Livro livroPorTitulo = null;
+
+        if(!livros.isEmpty()) {
+            for (Livro k : livros) {
+                if(k.getTitle().equalsIgnoreCase(titulo)) {
+                    livroPorTitulo = k;
+                    break;
+                }
+            }
+        }
+        return livroPorTitulo;
+
+
+    }
+
+    public static void main(String[] args) {
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2021);
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 2", 2020);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 2022);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 3", 2023);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 4", 1994);
+
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 4"));
+        System.out.println(catalogoLivros.pesquisaPorIntervaloAnos(2020, 2022));
+        System.out.println(catalogoLivros.pesquisaPorTitulo("Livro 1"));
+
+    }
+
 }
